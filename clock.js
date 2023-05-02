7db8610e4eb791083b932ad5303a9171139554c0
KileverTM <leko0223@gmail.com>
2023-05-02 14:04:10 +0200

    Working security with user input
    Working security with user input in console with wrong data given, working number limit to hours, minutes, seconds and working distinction between AM/PM value

--- clock.js
+++ clock.js
@@ -1,5 +1,3 @@
-
-
 const currentTime = document.querySelector("h1");
 content = document.querySelector(".content"),
 selectMenu = document.querySelectorAll("select"),
@@ -7,11 +5,7 @@ Console = document.querySelector("comment"),
 setAlarmBtn = document.querySelector("AlarmButt");
 stopAlarmBtm = document.querySelector("AlarmSTButt");
 
-
-
-
-
-let alarmTime, x,
+let alarmTime,text1, text2 , text3,
 ringtone = new Audio("./ringtone.mp3");
 
 for (let i = 12; i > 0; i--) {
@@ -34,7 +28,7 @@ for (let i = 2; i > 0; i--) {
 }
 
 
-for (let i = 11; i > -1; i--) {
+for (let i = 23; i > -1; i--) {
     i = i < 10 ? `0${i}` : i;
     let option = `<option value="${i}">${i}</option>`;
     selectMenu[3].firstElementChild.insertAdjacentHTML("afterend", option);
@@ -82,47 +76,111 @@ function setAlarm() {
     if (time.includes("Hour") || time.includes("Minute") || time.includes("AM/PM")) {
         return alert("Please, select a valid time to set Alarm!");
     }
-    alarmTime = time;
+    alarmTime = time;    
     
-    console.log (alarmTime);
     content.classList.add("disable");
    
 }
 function stopAlarm(){  
         alarmTime = "";
         ringtone.pause();
-        content.classList.remove("disable");
-        document.getElementById("comment").value = "";             
+        content.classList.remove("disable");                 
+}
+
+function checkData1Alarm(str)
+ {
+let testValue = 0;
+    for (let i = 12; i > 0; i--){
+        if (str == i){
+             testValue = 1;
+        }
+    }
+         if (testValue == 1){
+            return /^[0-9]+$/.test(str);
+         }else{
+            return false;
+              }
+};
+
+function checkData2Alarm(str)
+ {  
+ let testValue = 0;  
+    for (let i = 59; i > 0; i--){       
+        if (str == i){                  
+             testValue = 1;
+        }
+    }
+         if (testValue == 1){
+            return /^[0-9]+$/.test(str);
+         }else{
+            return false;
+              }
+}
+function checkData3Alarm(str){
+    
+    let testValue = 0;
+    for (let i = 2; i > 1; i--){
+        if (str == "AM"){
+             return true;
+        }else if (str == "PM"){
+            return true;
+        }else{
+        return false;
+        }
+
+    }
+        
 }
 
 function TestConsole() {  
 
+
     x = document.getElementById("comment").value;
+    let STRlength = x.length + 1;
     
     let y = "alarm";
     let w = "timer";
-    let z = x.slice(4, 9);  
+    let t = "set";
+    let v = x.slice(STRlength-19,STRlength-16);
+    let z = x.slice(STRlength-15, STRlength-10); 
+    text1 = x.slice(STRlength-9,STRlength-7);
+    text2 = x.slice(STRlength-6,STRlength-4);
+    text3 = x.slice(STRlength-3,STRlength-1);
 
-    console.log(z); 
-    console.log(y); 
     
-     if (z == y){
-     console.log("Działający if ")
-        console.log(x.substring(4, 10)); 
-    
-    
-        content.classList.add("disable");
-        console.log(x.slice(10, 18)); 
-        alarmTime = x.slice(10, 18);  
-    }else if(z == w) {
-    
-     
-    
-    }else {
-    alert("Syntax Error Check your syntax if it has errors");
-    }
-    
-   
+
+
+
+    if (v == t){
+
+        if(z == w) { 
+
+
+            }else if (z == y){
+                 
+                if(checkData1Alarm(text1)){
+                    
+                     if(checkData2Alarm(text2)){
+
+                       if(checkData3Alarm(text3)){
+                            content.classList.add("disable");    
+                            alarmTime = x.slice(STRlength-9, STRlength-1);                
+                            document.getElementById("comment").value = x + '\n';                 
+                         }else {alert("Syntax Error Check if AM/PM value is good");
+                         document.getElementById("comment").value = x + '\n';  }
+                    }else { alert("Syntax Error Check if minutes value is good");
+                    document.getElementById("comment").value = x + '\n';  }
+                }else{ alert("Syntax Error Check if hours value is good");
+            document.getElementById("comment").value = x + '\n'; }                
+
+          }else{            
+                 alert("Syntax Error Check your syntax if it has errors");
+                 document.getElementById("comment").value = x + '\n'; 
+             }
+   }else{    
+    alert("Syntax Error Check your syntax. Maybe you ment set");
+    document.getElementById("comment").value = x + '\n'; 
+   }
 
   }
 
